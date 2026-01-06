b04590a93bf170288f089dd7dc73b710d1f82eda
Relax-29 <relax9690@gmail.com>
2026-01-06 21:38:43 +0530

    Add Navbar functionality

---
+++ client/src/context/AppContext.jsx
@@ -0,0 +1,24 @@
+import { createContext, useState, useContext } from "react";
+import { useNavigate } from "react-router-dom";
+
+export const AppContext = createContext();
+
+export const AppContextProvider = ({ children }) => {
+    const navigate = useNavigate();
+    const [user, setUser] = useState(null);
+    const [isSeller, setIsSeller] = useState(null);
+        const [showUserLogin, setShowUserLogin] = useState(null);
+
+
+    const value = { navigate, user, setUser, isSeller, setIsSeller, showUserLogin, setShowUserLogin };
+
+    return (
+        <AppContext.Provider value={value}>
+            {children}
+        </AppContext.Provider>
+    );
+};
+
+export const useAppContext = () => {
+    return useContext(AppContext);
+};
