5f5d9b9570fc78c3049dd1113eceaf26844d1d75
Relax-29 <relax9690@gmail.com>
2026-01-07 07:50:05 +0530

    Complete CATEGORY and half complete BEST SELLER section

--- client/src/context/AppContext.jsx
+++ client/src/context/AppContext.jsx
@@ -1,16 +1,28 @@
-import { createContext, useState, useContext } from "react";
+import { createContext, useState, useContext, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
+import { dummyProducts } from "../assets/assets";
 
 export const AppContext = createContext();
 
 export const AppContextProvider = ({ children }) => {
+    const currency = import.meta.VITE_CURRENCY
     const navigate = useNavigate();
     const [user, setUser] = useState(null);
     const [isSeller, setIsSeller] = useState(null);
-        const [showUserLogin, setShowUserLogin] = useState(null);
+    const [showUserLogin, setShowUserLogin] = useState(null);
+    const [products, setProducts] = useState([])
+
+    const fetchProducts = async () => {
+        setProducts(dummyProducts);
+    }
+
+    useEffect(() => {
+        fetchProducts();
+    },[])
 
 
-    const value = { navigate, user, setUser, isSeller, setIsSeller, showUserLogin, setShowUserLogin };
+
+    const value = { navigate, user, setUser, isSeller, setIsSeller, showUserLogin, setShowUserLogin, currency, products };
 
     return (
         <AppContext.Provider value={value}>
